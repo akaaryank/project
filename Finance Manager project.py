f25fd8df46e693ec9171a5442dda40e463fe5429
akaaryank <152780531+akaaryank@users.noreply.github.com>
2024-03-16 14:43:31 +0530

    Update Finance Manager project.py

--- Finance Manager project.py
+++ Finance Manager project.py
@@ -23,15 +23,25 @@ def view_expenses(expenses):
         print(total_expense )
     print("=====================")
     print(f"Total Expense:\t{total_expense}")
-
+    
+def percent_exp(expenses):
+    total_expense = sum(expenses.values())
+    percentages = {}
+    for category, amount in expenses.items():
+        percentages[category] = (amount / total_expense) * 100
+        print(f"{category}:\t{percentages[category]}%")
+        
+        
 def main():
     expenses = {}
+    
     while True:
         print("\nExpense Tracker")
         print("1. Add Expense")
         print("2. View Expenses")
         print("3. Remove Expense")
-        print("4. Exit")
+        print("4. View Percentage of expenses")
+        print("5. Exit")
         choice = input("Enter your choice: ")
         
         if choice == '1':
@@ -46,20 +56,22 @@ def main():
                 view_expenses(expenses)
         elif choice == '3':
             category = input("Enter expense category: ")
-            category.lower()
             amount = float(input("Enter expense amount: "))
             sub_expense(expenses, category, amount)
             total_expense=total_expense-amount
             print("Expense removed successfully.!")
         elif choice == '4':
+           print("The percentage stats are:\n")
+           percent_exp(expenses)
+    
+        elif choice == '5':
             print("Exiting...")
             print("This Finance manager is made by Aaryan Khandelwal")
             print("Thank you, Please come again")
             break
         else:
-            print("Please enter a valid choice number.!!!!")
-            
-print("Welcome to Finance Manager")
+            print("Invalid choice. Please try again.")
 
+print("Welcome to Finance Manager")
 if __name__ == "__main__":
     main()
